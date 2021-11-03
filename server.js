e273c8f5f6363a00b415701f3c2301e8e39f9ff7
Lucas Grillos <LucasGrillos@gmail.com>
2021-11-03 10:01:44 -0700

    Trying to see if the  error is simply freecodecamp side

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
+/*
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.route('/_api/package.json')
@@ -54,7 +55,7 @@ app.use(function(err, req, res, next) {
       .send(err.message || 'SERVER ERROR');
   }
 })
-
+*/
 //Listen on port set in environment variable or default to 3000
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
