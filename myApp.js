44f6d2f75dd5d8172485d5c06e7363ba8b34d9e3
Lucas Grillos <LucasGrillos@gmail.com>
2021-11-03 10:30:29 -0700

    changed app.get method to use res.sendFile method to send our index.html file

--- myApp.js
+++ myApp.js
@@ -3,11 +3,15 @@ var app = express();
 //bGround.log("Hello World");
 console.log("Hello World");
 
-
+/*
 app.get("/", (req, res) => {
   res.send("Hello Express");
 })
+*/
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
 
 
 
